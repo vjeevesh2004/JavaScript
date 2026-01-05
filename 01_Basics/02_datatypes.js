cd70518c1e57c81498411a6e27fdd18c1776e206
vjeevesh2004 <166405802+vjeevesh2004@users.noreply.github.com>
2026-01-05 02:03:43 +0000

    datattypes done

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // to treat all JS code as newer version
+
+/*
+alert(3 + 3)
+will print error, we are using nodejs, not browser
+*/
+
+// console.log(3+3) console.log("Hitesh") // will return error
+console.log(3+3); console.log("Hii") // will print the output but not a good practice to do so
+
+// code readibility should be high
+
+let name = "ss"
+let age = 81
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean=> true/false
+// null => standalone value 
+// undefined => variable declared, value not defined
+// symbol => to show unqiueness 
+
+
+// object
+console.log(typeof age);
