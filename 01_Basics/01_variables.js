df2edf50ed49d71c0e563311d5b45dfd841fc48c
vjeevesh2004 <166405802+vjeevesh2004@users.noreply.github.com>
2026-01-05 01:44:21 +0000

    4th done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "hello@google.com"
+var accountPassword = "12234"
+accountCity = "Jaipur"
+
+// accountId = 2, cant change this
+
+console.log(accountId);
+accountEmail = "hchc"
+accountPassword = "212121212"
+accountCity = "RBC"
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+// in case of variable we can decalare variables using both let and var, 
+// normally var is not used generally coz od scope 
