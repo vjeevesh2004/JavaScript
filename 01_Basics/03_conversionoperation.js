4f0626e3ccfa7a3a78762d9446a5887671748e19
vjeevesh2004 <166405802+vjeevesh2004@users.noreply.github.com>
2026-01-06 17:00:25 +0000

    coversion_opt

---
+++ 01_Basics/03_conversionoperation.js
@@ -0,0 +1,11 @@
+let score = 33
+
+// const {score} = req.body // this won't tell u abt the number u hv recevied
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score) // this properly confirms that given value is a number
+console.log(typeof valueInNumber);
+
+
