dfec10dd1e22896fc2139a8abc273479c6ba408a
vjeevesh2004 <166405802+vjeevesh2004@users.noreply.github.com>
2026-01-07 01:53:59 +0000

    conversion_of_datatypes

--- 01_Basics/03_conversionoperation.js
+++ 01_Basics/03_conversionoperation.js
@@ -1,11 +1,27 @@
-let score = 33
+// let score = "33bac"
 
-// const {score} = req.body // this won't tell u abt the number u hv recevied
+// // const {score} = req.body // this won't tell u abt the number u hv recevied
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score) // this properly confirms that given value is a number
-console.log(typeof valueInNumber);
+// let valueInNumber = Number(score) // this properly confirms that given value is a number
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // will tell u abt the number we have, here we have NaN(NOt a number)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = true
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// // 1 => true; 0 >= false
 
 
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
