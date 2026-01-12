58f584beea7e75884afc1009c970ddfec9dfa2c0
vjeevesh2004 <166405802+vjeevesh2004@users.noreply.github.com>
2026-01-12 02:27:30 +0000

    operations done

--- 01_Basics/03_conversionoperation.js
+++ 01_Basics/03_conversionoperation.js
@@ -21,7 +21,58 @@
 // // 1 => true; 0 >= false
 
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// **************************************** OPERATIONS **************************************** //
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// // console.log(2%3);
+
+// let str1 = "hel"
+// let str2 = " jee"
+// str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); // will result 12
+// console.log(1 + "2"); // will result 12 
+// // thing is to think abt how does these string gets converted into numbers and get operated
+
+// console.log("1" + "2" + 2); // 122, we should get sum of all these but we get 4
+
+// console.log(1 + 2 + "2"); // 32, 1st two numbers sum and then string opt 
+
+// if string is in first then sab ko as a string treat kia jaega 
+// if string is at last then the further opt will occur then at last str will be added
+
+// console.log(true); // true
+// console.log(+true); // 1
+// console.log(true +); // error
+// console.log(+""); // 0
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2 + 2
+
+
+// let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter); // 101
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter); // 101
+
+
+
+
+
