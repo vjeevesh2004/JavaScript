f163ea06d8d46da7392255770af3057260979d4a
vjeevesh2004 <166405802+vjeevesh2004@users.noreply.github.com>
2026-02-10 02:46:13 +0000

    datatype_summary

--- 01_Basics/datatypes_summart.js
+++ 01_Basics/datatypes_summart.js
@@ -1,9 +1,6 @@
 // Primitive
 // 7 types: string, number, boolean, null, undefined, bigint
 
-// Non - Primitive (Reference Type)
-// Array, objects, functions
-
 const score = 100
 const scoreValue = 100.3
 
@@ -14,3 +11,26 @@ const outsideTemp = null
 
 // let userEmail; // undefined
 let userEmail = undefined; 
+
+// const bigNumber = 787878247809572839579425704n
+
+
+// Non - Primitive (Reference Type)
+// Array, objects, functions
+
+// array 
+const heroes = ["shaktiman", "naagaraj", "doga"]
+
+// objects
+let myObj = {
+    name : "roman", 
+    age: 68
+}
+
+// function
+const myFunction = function(){
+    console.log("Hello");
+}
+
+// to detect the type of datatypes
+
