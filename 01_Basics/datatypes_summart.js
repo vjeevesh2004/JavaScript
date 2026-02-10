200f84670823b7b8c59dcc8aad8bda5b6e0f9652
vjeevesh2004 <166405802+vjeevesh2004@users.noreply.github.com>
2026-02-10 02:30:56 +0000

    datatype_summary_done

---
+++ 01_Basics/datatypes_summart.js
@@ -0,0 +1,16 @@
+// Primitive
+// 7 types: string, number, boolean, null, undefined, bigint
+
+// Non - Primitive (Reference Type)
+// Array, objects, functions
+
+const score = 100
+const scoreValue = 100.3
+
+const LoggedIn = false
+const outsideTemp = null
+
+/* both will give undefined */
+
+// let userEmail; // undefined
+let userEmail = undefined; 
